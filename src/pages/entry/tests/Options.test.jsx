d6eaad1671c91a0058f3fe7d4aae532ef2882180
48209-Garip-Federico <garip.federico@gmail.com>
2022-11-23 15:10:44 -0300

    '52-Await-FindBy'

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -6,10 +6,10 @@ test('displays image for each scoop option from server', () => {
   render(<Options optionType="scoops" />);
 
   // Find images
-  // const scoopImages = screen.getByRole('img', { name: /scoop$/i });
-  // expect(scoopImages).toHaveLength(2);
+  const scoopImages = screen.getByRole('img', { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
 
   // confirm alt text
-  // const altText = scoopImages.map((element) => element.alt)
-  // expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
+  const altText = scoopImages.map((element) => element.alt)
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
 });
