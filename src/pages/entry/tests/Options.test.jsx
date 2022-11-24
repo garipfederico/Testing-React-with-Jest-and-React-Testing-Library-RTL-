9674a464ae1347df3915c5db44e7b3f081cc86b2
48209-Garip-Federico <garip.federico@gmail.com>
2022-11-24 16:52:23 -0300

    'Video53-InterceptandoLLamada-Axios'

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,11 +2,16 @@ import { render, screen } from '@testing-library/react';
 
 import Options from '../Options';
 
-test('displays image for each scoop option from server', () => {
+// test('displays image for each scoop option from server', () => {
+  // ver nota *1
+test('displays image for each scoop option from server', async() => {
   render(<Options optionType="scoops" />);
 
   // Find images
-  const scoopImages = screen.getByRole('img', { name: /scoop$/i });
+  // const scoopImages = screen.getByRole('img', { name: /scoop$/i });
+  // *1  La linea anterior se reemplaza con la siguiente para resolver problemas de sincronia
+  //     segun video 53 minuto 2 segundo 15
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   // confirm alt text
