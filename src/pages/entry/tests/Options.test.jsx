c8f2dbb1b5ce0131f47001991223be653c2cf535
48209-Garip-Federico <garip.federico@gmail.com>
2022-11-25 12:04:40 -0300

    '54-Topping-Option-from-server'

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -3,8 +3,8 @@ import { render, screen } from '@testing-library/react';
 import Options from '../Options';
 
 // test('displays image for each scoop option from server', () => {
-  // ver nota *1
-test('displays image for each scoop option from server', async() => {
+// ver nota *1
+test('displays image for each scoop option from server', async () => {
   render(<Options optionType="scoops" />);
 
   // Find images
@@ -15,6 +15,20 @@ test('displays image for each scoop option from server', async() => {
   expect(scoopImages).toHaveLength(2);
 
   // confirm alt text
-  const altText = scoopImages.map((element) => element.alt)
-  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
+  const altText = scoopImages.map(element => element.alt);
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays image for each topping options from server', async () => {
+  render(<Options optionType="toppings" />);
+  // Find images and expect that servermock returns 3 images
+  const images = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  const imageTitles = images.map(img => img.alt);
+  expect(imageTitles).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
 });
