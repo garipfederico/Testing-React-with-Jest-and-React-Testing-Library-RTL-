039d6c0021a34a103f015215e21842f9f35b608f
48209-Garip-Federico <garip.federico@gmail.com>
2022-11-26 08:48:36 -0300

    '59.-Running-only-Selected-Tests,-and-waitFor'

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import OrderEntry from '../OrderEntry';
 import { rest } from 'msw';
 import { server } from '../../../mocks/server';
 
+// test.only('handles error for scoops and toppings routes', async () => {
 test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
@@ -14,9 +15,12 @@ test('handles error for scoops and toppings routes', async () => {
   );
   render(<OrderEntry />);
 
-  const alerts = await screen.findAllByRole('alert', {
-    name: 'An unexpected error ocurred. Please try again later.',
+  await waitFor(async () => {
+    // const alerts = await screen.findAllByRole('alert', {name:'An unexpected error ocurred. Please try again later.'});
+    const alerts = await screen.findAllByRole('alert');
+    expect(alerts).toHaveLength(2);
   });
-
-  expect(alerts).toHaveLength(2);
 });
+
+// test('not a real test', () => {});
+// test.skip('not a real test', () => {});
