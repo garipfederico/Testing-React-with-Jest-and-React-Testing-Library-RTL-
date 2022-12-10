d46010f0287c852094d966ce99e564c5b49bd0cc
48209-Garip-Federico <garip.federico@gmail.com>
2022-12-10 12:04:05 -0300

    '66.-OPTIONAL-React-Code:-Use-Context-to-Display-Scoops-Subtotal'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import SummaryForm from './pages/summary/SummaryForm';
 import Options from './pages/entry/Options';
+import { Container } from 'react-bootstrap';
+import OrderEntry from './pages/entry/OrderEntry';
+import { OrderDetailsProvider } from './context/OrderDetails';
 
 function App() {
   return (
     <div className="App">
-    <Options></Options>
-      <SummaryForm />
+      {/* <Options></Options>
+      <SummaryForm /> */}
+      <Container>
+        <OrderDetailsProvider>
+          {/* Sumary page and entry page need provider*/}
+        <OrderEntry />
+        </OrderDetailsProvider>
+      </Container>
     </div>
   );
 }
