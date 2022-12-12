e02bf428bd7251e71afcbe88abb11a7c8aec186a
48209-Garip-Federico <garip.federico@gmail.com>
2022-12-12 13:15:06 -0300

    67.-Adding-Context-to-Test-Setup

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,28 +1,33 @@
-import { render, screen } from 'testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
+import { OrderDetailsProvider } from '../../../context/OrderDetails';
 
-test('update scoop subtotal when sxoops change', async () => {
-    const user = userEvent.setUp();
-  render(<Options optionType="scoops" />);
+test('update scoop subtotal when scoops change', async () => {
+  // const user = userEvent.setup();
+  
+  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
 
   // make sure total start out $0,00
   const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false });
 
   expect(scoopsSubtotal).toHaveTextContent('0.00');
 
-  //update vanilla sxoops to 1 and check the subtotal
-    const vanillaInput = await screen.findByRole('spinbutton', {name:'Vanilla'});
+  //update vanilla scoops to 1 and check the subtotal
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
 
-    await user.clear(vanillaInput)
-    await user.Type(vanillaInput,'1');
-    expect(scoopsSubtotal).toHaveTextContent('2.00');
+  await userEvent.clear(vanillaInput);
+  await userEvent.type(vanillaInput, '1');
+  expect(scoopsSubtotal).toHaveTextContent('2.00');
 
   //update chocolate scoops to 2 and check subtotal
-const chocolateInput = await screen.findByRole('spinbutton', {name:'Chocolate'});
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
 
-await user.clear(chocolateInput);
-await user.Type(chocolateInput,'2');
-expect(scoopsSubtotal).toHaveTextContent("6.00");
-
+  await userEvent.clear(chocolateInput);
+  await userEvent.type(chocolateInput, '2');
+  expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
